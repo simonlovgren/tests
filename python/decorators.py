7b8f0ea8b5695f3a5af838cb836a4850b71b3841
Simon Lövgren <lovgren.simon@gmail.com>
2018-01-23 00:14:26 +0100

    Tested decorators

---
+++ python/decorators.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+# Decorators
+def SimpleDecorator( fun ):
+    def _SimpleDecorator( name ):
+        name = f'my dearest {name}'
+        return fun( name )
+    return _SimpleDecorator
+
+def ArgDecorator( title ):
+    def _ArgDecorator( fun ):
+        def __ArgDecorator( name ):
+            name = f'my dearest {title} {name}'
+            return fun( name )
+        return __ArgDecorator
+    return _ArgDecorator
+            
+# Base functions
+def printHello( name ):
+    print( f'Hello {name}' )
+
+@SimpleDecorator
+def printHello2( name ):
+    print( f'Hello {name}' )
+
+@ArgDecorator( 'Sir' )
+def printHello3( name ):
+    print( f'Hello {name}' )
+
+# Normal calls
+printHello( 'Anna' )
+printHello2( 'Bert' )
+printHello3( 'Wolfram' )
+
+# Test 'weird' calls
+helloDec = SimpleDecorator( printHello )
+helloDec( 'Anna' )
+
+SimpleDecorator( printHello )( 'Herbert' )
+
+ArgDecorator( 'Mr' )( printHello )( 'Simon' )
